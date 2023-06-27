feb04da96b0cc199c392723bc270321e38f1db89
Yhann Daniel <yhanndaniel@gmail.com>
2023-06-27 11:19:15 -0300

    create clients module and component

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'clients' },
+  {
+    path: 'clients',
+    loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
